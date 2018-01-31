03041e294ab3788020f0a214fd8d9ec065d2d55e
Tyler Mitchell <gravdanger510@gmail.com>
2018-01-31 15:34:52 -0500

    more styles, linked about image

---
+++ page-about.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Template Name: About Page
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package DownsOakland
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+      <div class="inner-container">
+
+        <h1 class="page-title">About Downs</h1>
+        <p class="page-intro">As the church of Jesus Christ, we are committed to becoming and being a place of transformation and possibility where we equip and excite people for the work of holistic ministry.</p>
+        <!-- <video class="site-video" controls="true"  src="https://player.vimeo.com/external/212286321.hd.mp4?s=f2187a3be078ff189be55ac6ce307e306793693e&profile_id=119"></video> -->
+        <iframe src="https://player.vimeo.com/video/212286321" width="735" height="413" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+
+      </div>
+      <section class="site-section">
+        <div class="inner-container">
+          <h2 class="section-title">Pastor Anthony</h2>
+          <div class="half-column">
+            <img class="site-image" src="http://downsoakland.org/20170430_13.jpg" />
+          </div>
+          <div class="half-column">
+            <p>
+              Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.
+            </p>
+            <p>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Sed posuere consectetur est at lobortis. Nullam id dolor id nibh ultricies vehicula ut id elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section class="site-section">
+        <h2 class="section-title">
+          Current Leadership
+        </h2>
+
+        <div class="current-leadership inner-container">
+          <div class="leader">
+            <h3 class="leader-title">Lorem Ipsum</h3>
+            <p class="leader-description">Maecenas sed diam eget risus varius blandit sit amet non magna.</h3>
+          </div>
+
+          <div class="leader">
+            <h3 class="leader-title">Lorem Ipsum</h3>
+            <p class="leader-description">Maecenas sed diam eget risus varius blandit sit amet non magna.</h3>
+          </div>
+
+          <div class="leader">
+            <h3 class="leader-title">Lorem Ipsum</h3>
+            <p class="leader-description">Maecenas sed diam eget risus varius blandit sit amet non magna.</h3>
+          </div>
+        </div>
+
+
+      </section>
+      <section class="site-section">
+        <div class="inner-container">
+          <h2 class="section-title">History of Downs</h2>
+          <div class="half-column">
+            <p>
+              Downs Memorial was first established as Golden Gate Methodist Episcopal in 1890 and was located at 57th and Gaskill Streets in Oakland.  In 1916 the church moved to San Pablo and 56th Streets.  The present site was purchased in 1924 and the church was completed in 1925; Fuller Hall was added in 1940.  Golden Gate Methodist Episcopal closed in 1946, but reopened in 1948 to accommodate the large numbers of African-American men and women who returned to the East Bay after WWII.
+            </p>
+          </div>
+          <div class="half-column">
+            <p>
+              <img class="site-image" src="http://downsoakland.org/20170430_13.jpg" />
+            </p>
+          </div>
+        </div>
+        <div class="inner-container">
+          <div class="half-column">
+            <p>
+              <img class="site-image" src="http://downsoakland.org/20170430_13.jpg" />
+            </p>
+          </div>
+          <div class="half-column">
+            <p>
+              In 1948 the church was renamed Downs Memorial Methodist Church in honor of Reverend Karl Downs, president of Houston-Tillotson University in Austin, Texas., and renamed again as Downs Memorial United Methodist Church in 1968.  Our church has ministered to the African-American community of the San Francisco Bay Area for more than 126 years and continues to provide a spiritual home for many
+            </p>
+          </div>
+        </div>
+      </section>
+
+
+
+
+
+
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
